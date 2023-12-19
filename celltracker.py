efc480419665f38643116edd4d11f98cd87ad28e
marionhardy <126153921+marionhardy@users.noreply.github.com>
2023-12-18 21:21:46 -0800

    Inital sync with the gitignore file

---
+++ celltracker.py
@@ -0,0 +1,38 @@
+## Cell segmentation CellTracker
+
+#%% Set up
+
+# set up the github repository
+# i need to figure out how to exclude docs in the gitignore
+# ctrl shift p to select the celltracker conda interpreter
+
+
+#%%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
